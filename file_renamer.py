87f52b5240b83951688f7522e536641842d9c1bb
crypdough.eth <106392730+crypdoughdoteth@users.noreply.github.com>
2023-07-14 11:56:42 -0400

    Update file_renamer.py

--- file_renamer.py
+++ file_renamer.py
@@ -1,15 +1,15 @@
 import os 
 
+
 for file in os.listdir():
     
-    for file in os.listdir():
             #arbitrary alterations here
 
-        if file == "file_renamer.py":
+    if file == "file_renamer.py":
             print (file)
             continue
             
-        else: 
-            new_name = file.removeprefix('Open MInds Batch 1_')
-            os.rename(file, new_name)
-            print (new_name)
+    else: 
+        new_name = file.removeprefix('Open MInds Batch 1_')
+        os.rename(file, new_name)
+        print (new_name)
