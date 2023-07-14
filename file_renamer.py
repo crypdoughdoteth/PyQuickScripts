0dd29fd334077fe04cb3eab214e3c75cb32ffad0
crypdough.eth <106392730+crypdoughdoteth@users.noreply.github.com>
2023-07-14 11:56:00 -0400

    Update file_renamer.py

--- file_renamer.py
+++ file_renamer.py
@@ -1,19 +1,15 @@
 import os 
 
-
 for file in os.listdir():
-
-    json_file_path = f'C:\\Users\\{file}'
     
     for file in os.listdir():
             #arbitrary alterations here
 
-        if file == "jsonslicer.py":
+        if file == "file_renamer.py":
             print (file)
             continue
             
         else: 
-            new_name = file.split('1_')
-            modified_name = new_name[0] + new_name[1]
-            os.rename(file, modified_name)
-            print (modified_name)
+            new_name = file.removeprefix('Open MInds Batch 1_')
+            os.rename(file, new_name)
+            print (new_name)
